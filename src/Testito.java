88a3af5c9801f529b929500e56a604f7e7da78c7
hcalquin <hacz@hotmail.com>
2014-06-04 12:33:31 -0400

    extraccion datos de execblock

--- src/Testito.java
+++ src/Testito.java
@@ -1,11 +1,16 @@
 import alma.asdm.ASDM;
-import alma.asdm.ASDMTable;
 import alma.asdm.AntennaRow;
 import alma.asdm.AntennaTable;
+import alma.asdm.ExecBlockTable;
+import alma.asdm.FeedTable;
+import alma.asdm.FieldRow;
+import alma.asdm.FieldTable;
 import alma.asdm.MainRow;
+import alma.asdm.ReceiverRow;
+import alma.asdm.ReceiverTable;
 import alma.asdm.SourceRow;
-import alma.asdm.MainTable;
 import alma.asdm.ExecBlockRow;
+import alma.asdm.SourceTable;
 import alma.hla.runtime.asdm.ex.ConversionException;
 
 
@@ -13,50 +18,65 @@ public class Testito {
 
         public static final String ASDM_TEST_PATH = "uid___A002_X551c61_X1";
 
-        public static void main (String[] args) throws ConversionException {
+        public static void main (String[] args) throws ConversionException, IllegalAccessException {
                 ASDM asdm = ASDM.getFromXML(ASDM_TEST_PATH);
 
-                /*
-                for (SourceRow row: asdm.getSource().get()) {
-                        //System.out.println(row.getSourceName());
-                }
-				*/
-
-
-
-                for (ExecBlockRow row: asdm.getExecBlock().get()) {
-
-                    row.getAntennaId();
-
-                    //AntennaTable antenna_table = asdm.getAntenna().getRowByKey(row.getAntennaId());
                 
+                
+                // Obtencion de ExecBlockTable
+        		ExecBlockTable execBlockTable = asdm.getExecBlock();
+        		ExecBlockRow execBlockRow;
+        		
+        		/*
+                // Obtencion de FieldTable
+        		FieldTable fieldTable = asdm.getField();
+        		FieldRow fieldRow;
+                */
+        		        		       
+        		
+        		
+                for (MainRow mainRow: asdm.getMain().get()) {
+                	
+                	System.out.println("-----------");
+                                		                	
+                	
+                /*MAIN TABLE */
+                	
+	            		//access_estsize
+	            		System.out.println(mainRow.getDataSize());
+	            		
+	            	
+	            		//t_exptime
+	            		System.out.println(mainRow.getInterval());
+	            		
+            		
+	            		
+	            		
+	            	/*EXECBLOCK TABLE */
+		                	execBlockRow = execBlockTable.getRowByKey(mainRow.getExecBlockId());
+		                	
+		            		// obs_id
+		                    System.out.println(execBlockRow.getExecBlockUID());
+		                    
+		                    
+		                    // t_min
+		                    System.out.println(execBlockRow.getStartTime());
+		                    
+		           
+		                    // t_max
+		                    System.out.println(execBlockRow.getEndTime());
+		
+		
+		                    // em_min
+		                    System.out.println(execBlockRow.getBaseRangeMin());
+		
+		
+		                    // em_max
+		                    System.out.println(execBlockRow.getBaseRangeMax());
+		                    
 
-
-
-                    // obs_id
-                    System.out.println(row.getExecBlockUID());
-
-
-                    // t_min
-                    System.out.println(row.getStartTime());
-                    
-                    // t_max
-                    System.out.println(row.getEndTime());
-
-
-
-                    // em_min
-                    System.out.println(row.getBaseRangeMin());
-
-
-                    // em_max
-                    System.out.println(row.getBaseRangeMax());
-
-
+		                    	                    
                 }
-
-
-
 
 
         }
