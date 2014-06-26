cfba71999d14f04ce00426ba97a85be2e46d72a3
hcalquin <hacz@hotmail.com>
2014-06-25 21:05:02 -0400

    script manejo desde bash

---
+++ src/script_java_asdm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#javac -cp RUTA_LIBRERIAS/*  ruta_programa/Programa.java
+# ../lib* asume que librerias estan en la carpeta padre
+javac -cp .:../lib/* ASDMCsvConverter.java
+
+
+
+#javac -cp RUTA_LIBRERIAS/*  RUTA_PROGRAMA/Programa RUTA_DATOS_ASDM RUTA_SALIDA_CSV
+# ../lib* asume que librerias estan en la carpeta padre
+java -cp .:../lib/* ASDMCsvConverter ../ASDMData/ ../CSV/
+
