c74a4c3406a83a4987315ae7ce7582dd74e60db7
hcalquin <hacz@hotmail.com>
2014-08-29 21:56:16 -0400

    agregado: eliminacion de datos repetidos en archivo input.csv

--- src/script_java_asdm.sh
+++ src/script_java_asdm.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# agregar que extraiga los datos desde los xls
+
+
+# extrae elementos repetidos
+#awk -f ARCHIVO_CODIGO_AWK RUTA_DATOS_ENTRADA > RUTA_DATOS_SALIDA
+awk '!temp_array[$1]++' ../ASDMData/PorProcesar/input_bruto.csv > ../ASDMData/PorProcesar/input.csv
+
+
 #javac -cp RUTA_LIBRERIAS/*  ruta_programa/Programa.java
 # ../lib* asume que librerias estan en la carpeta padre
 javac -cp .:../lib/* Launcher.java
